9f128a5aa14308aa48c717d3fe2a73b782a443cd
baifan1366 <baifan1366@gmail.com>
2026-02-04 14:14:51 +0800

    feat: import tkinter

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 """
 Study Planner Application - With Timer Functionality and Tab Navigation
 """
+import tkinter as tk
+from tkinter import messagebox, ttk, Canvas
+
+class StudyPlannerApp:
+    def __init__(self):
+        """Initialize the Study Planner App"""
+        self.root = tk.Tk()
+        self.root.title("FocusFlow")
+        self.root.geometry("800x600")
+
+    def run(self):
+        """run app"""
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    app = StudyPlannerApp()
+    app.run()
